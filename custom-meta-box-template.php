2a227bda66fd525e79601e388ec5575942796b1f
Alex Mansfield <alex@sackclothstudios.com>
2013-10-18 21:40:21 +0200

    Fixed textdomain

--- custom-meta-box-template.php
+++ custom-meta-box-template.php
@@ -27,60 +27,60 @@ function prfx_meta_callback( $post ) {
 	?>
 
 	<p>
-		<label for="meta-text" class="prfx-row-title"><?php _e( 'Example Text Input', 'prfx-meta' )?></label>
+		<label for="meta-text" class="prfx-row-title"><?php _e( 'Example Text Input', 'prfx-textdomain' )?></label>
 		<input type="text" name="meta-text" id="meta-text" value="<?php if ( isset ( $prfx_stored_meta['meta-text'] ) ) echo $prfx_stored_meta['meta-text'][0]; ?>" />
 	</p>
 
 	<p>
-		<span class="prfx-row-title"><?php _e( 'Example Checkbox Input', 'prfx-meta' )?></span>
+		<span class="prfx-row-title"><?php _e( 'Example Checkbox Input', 'prfx-textdomain' )?></span>
 		<div class="prfx-row-content">
 			<label for="meta-checkbox">
 				<input type="checkbox" name="meta-checkbox" id="meta-checkbox" value="yes" <?php if ( isset ( $prfx_stored_meta['meta-checkbox'] ) ) checked( $prfx_stored_meta['meta-checkbox'][0], 'yes' ); ?> />
-				<?php _e( 'Checkbox label', 'prfx-meta' )?>
+				<?php _e( 'Checkbox label', 'prfx-textdomain' )?>
 			</label>
 			<label for="meta-checkbox-two">
 				<input type="checkbox" name="meta-checkbox-two" id="meta-checkbox-two" value="yes" <?php if ( isset ( $prfx_stored_meta['meta-checkbox-two'] ) ) checked( $prfx_stored_meta['meta-checkbox-two'][0], 'yes' ); ?> />
-				<?php _e( 'Another checkbox', 'prfx-meta' )?>
+				<?php _e( 'Another checkbox', 'prfx-textdomain' )?>
 			</label>
 		</div>
 	</p>
 
 	<p>
-		<span class="prfx-row-title"><?php _e( 'Example Radio Buttons', 'prfx-meta' )?></span>
+		<span class="prfx-row-title"><?php _e( 'Example Radio Buttons', 'prfx-textdomain' )?></span>
 		<div class="prfx-row-content">
 			<label for="meta-radio-one">
 				<input type="radio" name="meta-radio" id="meta-radio-one" value="radio-one" <?php if ( isset ( $prfx_stored_meta['meta-radio'] ) ) checked( $prfx_stored_meta['meta-radio'][0], 'radio-one' ); ?>>
-				<?php _e( 'Radio Option #1', 'prfx-meta' )?>
+				<?php _e( 'Radio Option #1', 'prfx-textdomain' )?>
 			</label>
 			<label for="meta-radio-two">
 				<input type="radio" name="meta-radio" id="meta-radio-two" value="radio-two" <?php if ( isset ( $prfx_stored_meta['meta-radio'] ) ) checked( $prfx_stored_meta['meta-radio'][0], 'radio-two' ); ?>>
-				<?php _e( 'Radio Option #2', 'prfx-meta' )?>
+				<?php _e( 'Radio Option #2', 'prfx-textdomain' )?>
 			</label>
 		</div>
 	</p>
 
 	<p>
-		<label for="meta-select" class="prfx-row-title"><?php _e( 'Example Select Input', 'prfx-meta' )?></label>
+		<label for="meta-select" class="prfx-row-title"><?php _e( 'Example Select Input', 'prfx-textdomain' )?></label>
 		<select name="meta-select" id="meta-select">
-			<option value="select-one" <?php if ( isset ( $prfx_stored_meta['meta-select'] ) ) selected( $prfx_stored_meta['meta-select'][0], 'select-one' ); ?>><?php _e( 'One', 'prfx-meta' )?></option>';
-			<option value="select-two" <?php if ( isset ( $prfx_stored_meta['meta-select'] ) ) selected( $prfx_stored_meta['meta-select'][0], 'select-two' ); ?>><?php _e( 'Two', 'prfx-meta' )?></option>';
+			<option value="select-one" <?php if ( isset ( $prfx_stored_meta['meta-select'] ) ) selected( $prfx_stored_meta['meta-select'][0], 'select-one' ); ?>><?php _e( 'One', 'prfx-textdomain' )?></option>';
+			<option value="select-two" <?php if ( isset ( $prfx_stored_meta['meta-select'] ) ) selected( $prfx_stored_meta['meta-select'][0], 'select-two' ); ?>><?php _e( 'Two', 'prfx-textdomain' )?></option>';
 		</select>
 	</p>
 
 	<p>
-		<label for="meta-textarea" class="prfx-row-title"><?php _e( 'Example Textarea Input', 'prfx-meta' )?></label>
+		<label for="meta-textarea" class="prfx-row-title"><?php _e( 'Example Textarea Input', 'prfx-textdomain' )?></label>
 		<textarea name="meta-textarea" id="meta-textarea"><?php if ( isset ( $prfx_stored_meta['meta-textarea'] ) ) echo $prfx_stored_meta['meta-textarea'][0]; ?></textarea>
 	</p>
 
 	<p>
-		<label for="meta-color" class="prfx-row-title"><?php _e( 'Color Picker', 'prfx-meta' )?></label>
+		<label for="meta-color" class="prfx-row-title"><?php _e( 'Color Picker', 'prfx-textdomain' )?></label>
 		<input name="meta-color" type="text" value="<?php if ( isset ( $prfx_stored_meta['meta-color'] ) ) echo $prfx_stored_meta['meta-color'][0]; ?>" class="meta-color" />
 	</p>
 
 	<p>
-		<label for="meta-image" class="prfx-row-title"><?php _e( 'Example File Upload', 'prfx-meta' )?></label>
+		<label for="meta-image" class="prfx-row-title"><?php _e( 'Example File Upload', 'prfx-textdomain' )?></label>
 		<input type="text" name="meta-image" id="meta-image" value="<?php if ( isset ( $prfx_stored_meta['meta-image'] ) ) echo $prfx_stored_meta['meta-image'][0]; ?>" />
-		<input type="button" id="meta-image-button" class="button" value="<?php _e( 'Choose or Upload an Image', 'prfx-meta' )?>" />
+		<input type="button" id="meta-image-button" class="button" value="<?php _e( 'Choose or Upload an Image', 'prfx-textdomain' )?>" />
 	</p>
  
 
