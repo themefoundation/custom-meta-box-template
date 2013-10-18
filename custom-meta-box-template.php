f29453f1caad6f83d6712eae0b0d384967db79a7
Alex Mansfield <alex@sackclothstudios.com>
2013-10-18 21:47:01 +0200

    Fixed textdomain

--- custom-meta-box-template.php
+++ custom-meta-box-template.php
@@ -188,8 +188,8 @@ function prfx_image_enqueue() {
 		wp_register_script( 'meta-box-image', plugin_dir_url( __FILE__ ) . 'meta-box-image.js', array( 'jquery' ) );
 		wp_localize_script( 'meta-box-image', 'meta_image',
 			array(
-				'title' => 'Choose or Upload an Image',
-				'button' => 'Use this image',
+				'title' => __( 'Choose or Upload an Image', 'prfx-textdomain' ),
+				'button' => __( 'Use this image', 'prfx-textdomain' ),
 			)
 		);
 		wp_enqueue_script( 'meta-box-image' );
